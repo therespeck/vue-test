8025b5d53557a89409e3e23553ea32374f498102
therespeck <therespeck@gmail.com>
2020-11-05 15:55:01 +0300

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <p>this is cutom test line</p>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
